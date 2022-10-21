20c21f973e40836499c0c4d634b09b7b86910b9c
Abderahmon <abdulrahmon.ola122@gmail.com>
2022-10-21 16:10:40 -0700

    new file

--- get_flags.c
+++ get_flags.c
@@ -4,7 +4,7 @@
  * get_flags - Calculates active flags
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
- * 
+ *
  * Return: Flags:
  */
 int get_flags(const char *format, int *i)
@@ -27,7 +27,7 @@ int get_flags(const char *format, int *i)
 
 		if (FLAGS_CH[j] == 0)
 			break;
-	}
+
 
 	*i = curr_i - 1;
 
